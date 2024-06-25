a2af259e7ab2b6458911c10905e9f6741273c24c
Toqa-ElMalah <mtoqa81@gmail.com>
2024-06-26 02:13:24 +0300

    Update lcd1.c

--- lcd1.c
+++ lcd1.c
@@ -1,119 +1,114 @@
-/*
- * lcd1.c
- *
- * Created: 10/14/2023 1:10:33 PM
- *  Author: DELL
- */ 
-
-#include "lcd1.h"
-#define  F_CPU 8000000UL
+ #include "LCD.h"
 #include <util/delay.h>
 
+#define  F_CPU 8000000UL
 
-void LCD_vInit (char port){
+void LCD_int (char Data_port)
+{
 	_delay_ms(200);
 	#if defined eight_bits_mode
-	DIO_set_port_direction(port,0xff);
-	DIO_vsetPINDir(port_EN,EN,1);
-	DIO_vsetPINDir(port_RW,RW,1);
-	DIO_vsetPINDir(port_RS,RS,1);
-	DIO_write(port_RW,RW,0);
-	LCD_vSend_cmd(port,EIGHT_BITS);
+	DIO_set_port_direction (Data_port,0xff);
+	DIO_set_pin_direction (RS_port,RS_pin ,1);
+	DIO_set_pin_direction(RW_port,RW_pin,1);
+	DIO_set_pin_direction (EN_port,EN_pin,1);
+	DIO_write_pin(RW_port,RW_pin,0);
+	LCD_send_command(Data_port,EIGHT_BITS); //8 bit mode
 	_delay_ms(1);
-	LCD_vSend_cmd(port,CURSOR_ON_DISPLAY_ON);
+	LCD_send_command(Data_port,CURSOR_ON_DISPLAN_ON);//display on cursor on
 	_delay_ms(1);
-	LCD_vSend_cmd(port,CLR_SCREEN);		
-	_delay_ms(10);	
-	LCD_vSend_cmd(port,ENTRY_MODE);
-	_delay_ms(1);
-	
-	#elif defined four_bits_mode
-	DIO_vsetPINDir(port,4,1);
-	DIO_vsetPINDir(port,5,1);
-	DIO_vsetPINDir(port,6,1);
-	DIO_vsetPINDir(port,7,1);
-	DIO_vsetPINDir(port_EN,EN,1);
-	DIO_vsetPINDir(port_RW,RW,1);
-	DIO_vsetPINDir(port_RS,RS,1);
-	DIO_write(port_RW,RW,0);
-	LCD_vSend_cmd(port,RETURN_HOME);
+	LCD_send_command(Data_port,CLR_SCREEN);//clear the screen
 	_delay_ms(10);
-	LCD_vSend_cmd(port,FOUR_BITS);
+	LCD_send_command(Data_port,ENTRY_MODE); //entry mode
 	_delay_ms(1);
-	LCD_vSend_cmd(port,CURSOR_ON_DISPLAY_ON);
-	_delay_ms(1);
-	LCD_vSend_cmd(port,CLR_SCREEN);
+	#elif defined four_bits_mode
+	DIO_set_pin_direction(Data_port,4,1);
+	DIO_set_pin_direction(Data_port,5,1);
+	DIO_set_pin_direction(Data_port,6,1);
+	DIO_set_pin_direction(Data_port,7,1);
+	DIO_set_pin_direction(EN_port,EN_pin,1);
+	DIO_set_pin_direction(RW_port,RW_pin,1);
+	DIO_set_pin_direction(RS_port,RS_pin,1);
+	DIO_write_pin(RW_port,RW_pin,1);
+	LCD_send_command(Data_port,RETURN_HOME); //return home
 	_delay_ms(10);
-	LCD_vSend_cmd(port,ENTRY_MODE);
+	LCD_send_command(Data_port,FOUR_BITS); //4bit mode
+	_delay_ms(1);
+	LCD_send_command(Data_port,CURSOR_ON_DISPLAN_ON);//display on cursor on
+	_delay_ms(1);
+	LCD_send_command(Data_port,CLR_SCREEN);//clear the screen
+	_delay_ms(10);
+	LCD_send_command(Data_port,ENTRY_MODE); //entry mode
 	_delay_ms(1);
 	#endif
 }
 
-static void enable(void){
-	DIO_write(port_EN,EN,1);
-	_delay_ms(2);
-	DIO_write(port_EN,EN,0);
-	_delay_ms(2);
-}
 
-void LCD_vSend_cmd (char port,char cmd){
+void LCD_send_char (char Data_port, char data)
+{
 	#if defined eight_bits_mode
-	DIO_write_port(port,cmd);
-	DIO_write(port_RS,RS,0);
-	enable();
-	
+	DIO_write_port (Data_port,data);
+	DIO_write_pin (RS_port ,RS_pin ,1);
+	enable ();
 	#elif defined four_bits_mode
-	write_high_nibble(port,cmd>>4);
-	DIO_write(port_RS,RS,0);
-	enable();
-	write_high_nibble(port,cmd);
-	DIO_write(port_RS,RS,0);
-	enable();
-	#endif
+	write_high_nibble(Data_port,data>>4);
+	DIO_write_pin (RS_port ,RS_pin ,1);
+	enable ();
+	write_high_nibble(Data_port,data);
+	DIO_write_pin (RS_port ,RS_pin ,1);
+	enable ();
 	_delay_ms(1);
+	#endif
+	
 }
 
-void LCD_vSend_char (char port,char data){
+static void enable (void)
+{
+	DIO_write_pin (EN_port ,EN_pin ,1);
+	_delay_ms (2);
+	DIO_write_pin (EN_port ,EN_pin ,0);
+	_delay_ms (2);
+}
+
+void LCD_send_command (char Data_port, char command)
+{
 	#if defined eight_bits_mode
-	DIO_write_port(port,data);
-	DIO_write(port_RS,RS,1);
-	enable();
-	
+	DIO_write_port (Data_port,command);
+	DIO_write_pin (RS_port ,RS_pin ,0);
+	enable ();
 	#elif defined four_bits_mode
-	write_high_nibble(port,data>>4);
-	DIO_write(port_RS,RS,1);
-	enable();
-	write_high_nibble(port,data);
-	DIO_write(port_RS,RS,1);
-	enable();
-	#endif
+	write_high_nibble(Data_port,command>>4);
+	DIO_write_pin (RS_port ,RS_pin ,0);
+	enable ();
+	write_high_nibble(Data_port,command);
+	DIO_write_pin (RS_port ,RS_pin ,0);
+	enable ();
 	_delay_ms(1);
+	#endif
+	
 }
 
-void LCD_vSend_string (char port,char * data){
-	while((* data) != '\0'){
-		LCD_vSend_char(port,* data);
-		data++;
-	}	
+void LCD_send_string (char Data_port, char *ptr)
+{
+	while (*ptr!=0)
+	{
+		LCD_send_char ( Data_port, *ptr);
+		*ptr++;
 	}
-	
-void LCD_clearscreen(char port){
-	LCD_vSend_cmd(port,CLR_SCREEN);
-	_delay_ms(10);
 }
 
-void LCD_movecursor (char port,char row,char coloumn){
+void LCD_move_coursor (char Data_port, char row, char column)
+{
 	char data;
-	if(row>2 || row<1 || coloumn>16 || coloumn<1){
-		data=0x80;
+	if (row==1 || row ==2)
+	{
+		if (row==1)
+		data= 0x80+column-1;
+		else
+		data= 0xc0+column-1;
 	}
-	else if (row==1){
-		data = 0x80+coloumn-1;
-	}
-	else if (row==2){
-		data = 0xc0+coloumn-1;
-	}
-	LCD_vSend_cmd(port,data);
-	_delay_ms(1);
+	else
+	data= 0x80;
 	
+	LCD_send_command(Data_port,data);
+	_delay_ms(1);
 }
