973c9b33503550b0f1f8a687806e768d274a06ed
Toqa-ElMalah <mtoqa81@gmail.com>
2024-06-26 02:11:07 +0300

    Update Keypad_Driver.h

--- Keypad_Driver.h
+++ Keypad_Driver.h
@@ -1,13 +1,6 @@
-/*
- * Keypad_Driver.h
- *
- * Created: 10/17/2023 11:09:45 PM
- *  Author: DELL
- */ 
-
-
 #ifndef KEYPAD_DRIVER_H_
 #define KEYPAD_DRIVER_H_
+
 #define NOTPRESSED 0xff
 
 void keypad_vInit (void);
